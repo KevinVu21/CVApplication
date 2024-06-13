98df57a81c7f5eb64095410457e8587da55bea7e
Kevin <kevu4776@gmail.com>
2024-06-13 00:53:07 -0500

    finished implementing add experience button

--- src/experience.jsx
+++ src/experience.jsx
@@ -3,36 +3,64 @@ function Experience({showExperience, experienceInfo, savedExperiences, handleAdd
     return(
       <div className='experience'>
         <h2>Experience</h2>
-        <label htmlFor="companyName">Company Name</label>
-        <input 
-        type='text'
-        id='companyName'
-        name='companyName'
-        />
-        <label htmlFor='companyStart'>Start Date</label>
-        <input
-        type='month'
-        id='companyStart'
-        name='companyStart'
-        />
-        <label htmlFor='comapnyEnd'>End Date</label>
-        <input
-        type='month'
-        id='companyEnd'
-        name='companyEnd'
-        />
-        <label htmlFor='experienceLocation'>Location</label>
-        <input
-        type='text'
-        id='experienceLocation'
-        name='experienceLocation'
-        />
-        <label htmlFor='jobDescription'>Description</label>
-        <input
-        type='text'
-        id='jobDescription'
-        name='jobDescription'
-        />
+        {showExperience ? (
+          <div className='experienceInput'>
+          <label htmlFor="companyName">Company Name</label>
+          <input 
+          type='text'
+          id='companyName'
+          name='companyName'
+          value={experienceInfo.companyName}
+          onChange={handleExperienceChange}
+          />
+          <label htmlFor='startDate'>Start Date</label>
+          <input
+          type='month'
+          id='startDate'
+          name='startDate'
+          value={experienceInfo.startDate}
+          onChange={handleExperienceChange}
+          />
+          <label htmlFor='endDate'>End Date</label>
+          <input
+          type='month'
+          id='endDate'
+          name='endDate'
+          value={experienceInfo.endDate}
+          onChange={handleExperienceChange}
+          />
+          <label htmlFor='location'>Location</label>
+          <input
+          type='text'
+          id='location'
+          name='location'
+          value={experienceInfo.location}
+          onChange={handleExperienceChange}
+          />
+          <label htmlFor='description'>Description</label>
+          <input
+          type='text'
+          id='description'
+          name='description'
+          value={experienceInfo.description}
+          onChange={handleExperienceChange}
+          />
+          <button onClick={handleSaveExperience} className='saveButton'>Save</button>
+          </div>): (
+            <div className='experienceInfo'>
+              {savedExperiences.map((savedExperience, index) => (
+            <div class='companyInformation' key={index}>
+              <div className='savedCompany'>
+                {savedExperience.companyName}
+              </div>
+                <button onClick={() => handleDeleteExperience(index)}>Delete</button>
+            </div>
+          ))}
+          <button onClick={handleAddExperience}>Add Experience</button>
+            </div>
+          )
+
+        }
       </div>
     )
 }
